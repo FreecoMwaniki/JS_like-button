6697a3240da48335014bdfcf05a3fbe5a6a4ebc4
FreecoMwaniki <freecomwaniki@gmail.com>
2024-10-25 10:18:09 +0300

    tried to display number of likes

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ let balance = [
   {
     id: 1,
     title: "YIN&YANG",
-    imageUrl: "https://cdn.pixabay.com/photo/2013/07/12/18/02/yin-and-yang-152829_640.png",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2013/07/12/18/02/yin-and-yang-152829_640.png",
     description: "LIGHT AND DARK",
-    likes: 0, 
+    likes: 0,
   },
   {
     id: 2,
     title: "SCALES",
-    imageUrl: "https://cdn.pixabay.com/photo/2024/05/12/10/05/libra-8756539_640.png",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2024/05/12/10/05/libra-8756539_640.png",
     description: "TWO SIDES DIFFERENT OUTCOMES",
     likes: 0,
   },
   {
     id: 3,
     title: "MONY",
-    imageUrl: "https://cdn.pixabay.com/photo/2023/05/11/11/05/glass-sphere-7986102_640.jpg",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2023/05/11/11/05/glass-sphere-7986102_640.jpg",
     description: "HARMONY ALL AROUND",
     likes: 0,
   },
@@ -25,19 +28,18 @@ let balance = [
 
 // ii inasva likes kwa localstorage.
 function saveLikesToLocalStorage() {
-  localStorage.setItem("likes", JSON.stringify(balance)); 
+  localStorage.setItem("likes", JSON.stringify(balance));
 }
 
 // inafetche na kuload likes kutoka localStorage
 function loadLikesFromLocalStorage() {
   const savedLikes = JSON.parse(localStorage.getItem("likes"));
   if (savedLikes) {
-    balance = savedLikes; 
+    balance = savedLikes;
   }
 }
 
 loadLikesFromLocalStorage();
-
 
 function displayBalance({ id, title, imageUrl, description, likes }) {
   return `
@@ -62,8 +64,9 @@ function increaseLike(id) {
   const item = balance.find((item) => item.id === id);
   if (item) {
     item.likes += 1;
-    document.getElementById(`like-count-${id}`).textContent = `${item.likes} Likes`; 
-    saveLikesToLocalStorage(); 
+    const likeCountElement = document.getElementById(`like-count-${id}`);
+    likeCountElement.textContent = `${item.likes} Likes`;
+    saveLikesToLocalStorage();
   }
 }
 
